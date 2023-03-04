67378bee80e660e809d9f66593703bb98e99b330
herrera-d <diego.herrera.ar@gmail.com>
2023-03-04 15:30:57 -0300

    update package.json to deploy on git-hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/alpaca-generator/",
-  build: {
-    outDir: "dist",
-    assetsDir: "src/assets",
-    minify: "esbuild",
-    sourcemap: true,
-  },
 });
